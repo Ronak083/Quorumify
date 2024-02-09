f0431da0ea4534ecb81ae746577f017cf429a954
ronak083 <ronakgupta083@gmail.com>
2024-02-09 19:51:28 +0530

    Change Origin

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-const apiURL = 'http://localhost:8080/api/';
+const apiURL = 'http://quorumify-aws-api-env-1.eba-3d3d9mxg.eu-north-1.elasticbeanstalk.com/api/';
 
 const apiService = axios.create({
     baseURL: apiURL,
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
         "Authorization": 'Bearer ' + localStorage.getItem('token'),
     }
 
